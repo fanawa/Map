a942ff0edc3f52dc30f42193091e1af77d7776cb
Hanawa Fumihito <fmff59@gmail.com>
2020-12-20 16:38:09 +0900

    1220 1630

--- app/src/main/java/com/example/map/MapsActivity.java
+++ app/src/main/java/com/example/map/MapsActivity.java
@@ -17,15 +17,17 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity {
+
 
     //public static final String API_KEY_map = "AIzaSyCAYumamZGODLCMJH1lVtEIai6qSkGGi_Y";
 
-    public static final String API_KEY = "AIzaSyDfpLhAh6x7VwCWOzmPVYRsLjiM4ZYJE1c";  //APIキー9
-    public static final String API_KEY2 = "AIzaSyAe5HZh70SjuIR12M9qusyPR-dOzSb_dCw";  //APIキー10
-    public static final String API_KEY3 = "AIzaSyBYI70DxljUAm6rBcV8p_I3zmBjg4Sbibs";    //APIキー11
-    public static final String API_KEY4 = "AIzaSyDIh0Ohakgcv2wuCm0isarw06tVLkk4sjo";    //APIキー12
-    public static final String API_KEY5 = "AIzaSyAmfg3dDX1v1bRaDatw-yjqXNyd_C06d-A";    //APIキー13
+    public static final String API_KEY1 = "AIzaSyDfpLhAh6x7VwCWOzmPVYRsLjiM4ZYJE1c";  //APIキー9 video1
+    public static final String API_KEY2 = "AIzaSyAe5HZh70SjuIR12M9qusyPR-dOzSb_dCw";  //APIキー10 video2
+    public static final String API_KEY3 = "AIzaSyBYI70DxljUAm6rBcV8p_I3zmBjg4Sbibs";    //APIキー11 video3
+    public static final String API_KEY4 = "AIzaSyDIh0Ohakgcv2wuCm0isarw06tVLkk4sjo";    //APIキー12 video4
+    public static final String API_KEY5 = "AIzaSyAmfg3dDX1v1bRaDatw-yjqXNyd_C06d-A";    //APIキー13 video5
 
     public static final String VIDEO_ID = "CQ6dbUmU__o";
 
@@ -33,16 +35,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public static final String VIDEO_ID2 = "8EZdi914dsU";         //薮地島
     public static final String VIDEO_ID3 = "XN_Gxf0d1oY";         //ウクの浜
     public static final String VIDEO_ID4 = "LOxURLadloE&t=1s";         //アクナ浜
-    public static final String VIDEO_ID5 ="AsKf_wNiqsc&t=26s";    //white beach
+    public static final String VIDEO_ID5 = "AsKf_wNiqsc&t=26s";    //white beach
 
 
-    private GoogleMap mMap;
+    private GoogleMap mMap1;
+    private GoogleMap mMap2;
+    private GoogleMap mMap3;
+    private GoogleMap mMap4;
+    private GoogleMap mMap5;
+
+//    private GoogleMap googleMap1;
+//    private GoogleMap googleMap2;
+//    private GoogleMap googleMap3;
+//    private GoogleMap googleMap4;
+//    private GoogleMap googleMap5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+
+        Log.i("log","Main_onCreate");
 /**
  * 1
  */
@@ -52,7 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         youTubePlayerFragment.initialize
                 (
-                        API_KEY,
+                        API_KEY1,
                         new YouTubePlayer.OnInitializedListener() {
                             @Override
                             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
@@ -79,14 +93,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             }
                         }
                 );
+        Log.i("log","Main_onCreate_video1");
         //video1 ここまで
 
         //map1
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map1);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap1 = googleMap;
+
+                // Add a marker in hamahiga and move the camera
+                LatLng hamahiga = new LatLng(26.31283, 127.96045);
+                mMap1.addMarker(new MarkerOptions().position(hamahiga).title("Marker in hamahiga"));
+                mMap1.moveCamera(CameraUpdateFactory.newLatLng(hamahiga));
+
+                //ズーム機能
+                UiSettings us = mMap1.getUiSettings();
+                us.setZoomControlsEnabled(true);
+
+            }
+        });
+        Log.i("log","Main_onCreate_map1");
         //map1ここまで
+
+
 
 /**
  * 2
@@ -124,13 +157,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             }
                         }
                 );
+        Log.i("log","Main_onCreate_video2");
         //video2 ここまで
 
         //map2
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment2 = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map2);
-        mapFragment2.getMapAsync(this);
+        mapFragment2.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap2) {
+                mMap2 = googleMap2;
+
+                // Add a marker in hamahiga and move the camera
+                LatLng yabuchi = new LatLng(26.322269, 127.917837);
+                mMap2.addMarker(new MarkerOptions().position(yabuchi).title("Marker in yabuchi"));
+                mMap2.moveCamera(CameraUpdateFactory.newLatLng(yabuchi));
+
+                //ズーム機能
+                UiSettings us = mMap2.getUiSettings();
+                us.setZoomControlsEnabled(true);
+
+            }
+        });
+        Log.i("log","Main_onCreate_map2");
         //map2ここまで
 
         /**
@@ -169,44 +219,157 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             }
                         }
                 );
+        Log.i("log","Main_onCreate_video3");
         //video3 ここまで
 
         //map3
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //26.367494617568482, 127.99358563080219
         SupportMapFragment mapFragment3 = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map3);
-        mapFragment.getMapAsync(this);
-        //map1ここまで
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap3) {
+                mMap3 = googleMap3;
+
+                // Add a marker in uku and move the camera
+                LatLng uku = new LatLng(26.367494617568482, 127.99358563080219);
+                mMap3.addMarker(new MarkerOptions().position(uku).title("Marker in uku"));
+                mMap3.moveCamera(CameraUpdateFactory.newLatLng(uku));
+
+                //ズーム機能
+                UiSettings us = mMap3.getUiSettings();
+                us.setZoomControlsEnabled(true);
+            }
+        });
+        Log.i("log","Main_onCreate_map3");
+        //map3ここまで
 
 
+        /**
+         * 4
+         */
+        //video4
+        YouTubePlayerFragment youTubePlayerFragment4 = (YouTubePlayerFragment) getFragmentManager()
+                .findFragmentById(R.id.fragment4);
 
-    }
+        youTubePlayerFragment4.initialize
+                (
+                        API_KEY4,
+                        new YouTubePlayer.OnInitializedListener() {
+                            @Override
+                            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+                                youTubePlayer.setFullscreenControlFlags(YouTubePlayer.FULLSCREEN_FLAG_CONTROL_ORIENTATION |
+                                        YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+                                if (!wasRestored) {
+                                    youTubePlayer.cueVideo(VIDEO_ID4);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(26.31283, 127.96045);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                                    Log.i("log", "onInitializationSuccess");
+                                }
+                            }
 
-        //ズーム機能
-        UiSettings us = mMap.getUiSettings();
-        us.setZoomControlsEnabled(true);
+                            @Override
+                            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult error) {
+                                final int REQUEST_CODE = 1;
+
+                                if (error.isUserRecoverableError()) {
+                                    error.getErrorDialog(MapsActivity.this, REQUEST_CODE).show();
+                                } else {
+                                    String errorMessage = String.format("There was an error initializing the YoutubePlayer (%1$s)", error.toString());
+                                    Toast.makeText(MapsActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                );
+        Log.i("log","Main_onCreate_video4");
+        //video4 ここまで
+
+        //map4
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //26.36386645542974, 127.994072544131
+        SupportMapFragment mapFragment4 = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map4);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap4) {
+                mMap4 = googleMap4;
+
+                // Add a marker in akuna and move the camera
+                LatLng akuna = new LatLng(26.36386645542974, 127.994072544131);
+                mMap4.addMarker(new MarkerOptions().position(akuna).title("Marker in akuna"));
+                mMap4.moveCamera(CameraUpdateFactory.newLatLng(akuna));
+
+                //ズーム機能
+                UiSettings us = mMap4.getUiSettings();
+                us.setZoomControlsEnabled(true);
+            }
+        });
+        Log.i("log","Main_onCreate_map4");
+        //map4ここまで
 
 
+        /**
+         * 5
+         */
+        //video5
+        YouTubePlayerFragment youTubePlayerFragment5 = (YouTubePlayerFragment) getFragmentManager()
+                .findFragmentById(R.id.fragment5);
 
+        youTubePlayerFragment5.initialize
+                (
+                        API_KEY5,
+                        new YouTubePlayer.OnInitializedListener() {
+                            @Override
+                            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+                                youTubePlayer.setFullscreenControlFlags(YouTubePlayer.FULLSCREEN_FLAG_CONTROL_ORIENTATION |
+                                        YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE);
 
+                                if (!wasRestored) {
+                                    youTubePlayer.cueVideo(VIDEO_ID5);
+
+                                    Log.i("log", "onInitializationSuccess");
+                                }
+                            }
+
+                            @Override
+                            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult error) {
+                                final int REQUEST_CODE = 1;
+
+                                if (error.isUserRecoverableError()) {
+                                    error.getErrorDialog(MapsActivity.this, REQUEST_CODE).show();
+                                } else {
+                                    String errorMessage = String.format("There was an error initializing the YoutubePlayer (%1$s)", error.toString());
+                                    Toast.makeText(MapsActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                );
+        Log.i("log","Main_onCreate_video5");
+        //video5 ここまで
+
+        //map5
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //26.311742704026422, 127.89030250840678
+        SupportMapFragment mapFragment5 = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map5);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap5) {
+                mMap5 = googleMap5;
+
+                // Add a marker in white beach and move the camera
+                LatLng w_b = new LatLng(26.311742704026422, 127.89030250840678);
+                mMap5.addMarker(new MarkerOptions().position(w_b).title("Marker in white beach"));
+                mMap5.moveCamera(CameraUpdateFactory.newLatLng(w_b));
+
+                //ズーム機能
+                UiSettings us = mMap5.getUiSettings();
+                us.setZoomControlsEnabled(true);
+            }
+        });
+        Log.i("log","Main_onCreate_map5");
+        //map5ここまで
 
     }
 }
